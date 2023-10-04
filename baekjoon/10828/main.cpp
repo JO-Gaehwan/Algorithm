b1b3b4daf840c6b105a2813fa209f01e80996373
JO-Gaehwan <146069282+JO-Gaehwan@users.noreply.github.com>
2023-10-04 22:58:08 +0900

    baekjoon10828

--- baekjoon/10828/main.cpp
+++ baekjoon/10828/main.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <sstream>
+#include <string>
+
 using namespace std;
 
 int stack[10000 + 10];
@@ -26,8 +29,46 @@ int pop() {
 }
 
 int size() { return pos; }
+
+int empty() {
+  if (pos == 0) {
+    return 1;
+  } else {
+    return 0;
+  }
+}
+
 int main() {
   int num = 0;
-  cin >> num;
-  cout << "number is " << num << endl;
+  // cin >> num; 개행문자 \n을 직후 getline에서 받아버린다.
+  scanf("%d\n", &num);
+  for (int i = 0; i < num; ++i) {
+    string cmd;
+    getline(cin, cmd);
+    stringstream ss(cmd);
+    string com;
+    int push_num = 0;
+
+    ss >> com;
+
+    if (com == "push") {
+      while (ss.peek() == ' ') {
+        ss.ignore();
+      }
+      if (ss >> push_num) {
+        push(push_num);
+      }
+    } else if (com == "top") {
+      cout << top() << endl;
+    } else if (com == "pop") {
+      cout << pop() << endl;
+    } else if (com == "empty") {
+      cout << empty() << endl;
+    } else if (com == "size") {
+      cout << size() << endl;
+    } else {
+      // do nothing
+    }
+  }
+  return 0;
 }
